4ed6c0a882bdf4b8eebec8139124b1c2fde31bfc
Suresh Sharma <112813768+SuRu190@users.noreply.github.com>
2025-08-05 19:41:53 +0530

    Add files via upload

---
+++ task13-User-Info-Card/script.js
@@ -0,0 +1,15 @@
+const userName = prompt("Enter your NAME");
+const userAge = prompt("Enter your AGE");
+const favColor = prompt("Enter your FAVOURITE COLOR");
+const hobby = prompt("Enter your HOBBY");
+const dreamJob = prompt("Enter your DREAM JOB");
+
+console.log("-----------------------------------------");
+console.log("              USER INFO CARD             ");
+console.log("-----------------------------------------");
+console.log(`Name                    : ${userName}`);
+console.log(`Age                     : ${userAge}`);
+console.log(`Fav Color               : ${favColor}`);
+console.log(`Hobby                   : ${hobby}`);
+console.log(`Dream Job               : ${dreamJob}`);
+console.log("-----------------------------------------");
